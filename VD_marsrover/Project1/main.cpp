e72eed480f5cdd412e4d309ba5c05cfe67a926b4
Móré Sándor <more.sandor@mechwart.local>
2026-02-19 11:20:38 +0100

    _ű: Initial commit

---
+++ VD_marsrover/Project1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include "raylib.h"
+#include "raymath.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+int main()
+{
+	InitWindow(WIDTH, HEIGHT, "Hello Raylib");
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+	CloseWindow();
+	std::cout << "Done\n";
+}
