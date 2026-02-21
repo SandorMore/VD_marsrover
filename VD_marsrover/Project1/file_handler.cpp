f877c5f06f5196eed26032863e3a8e84e4594e3d
SandorMore <msaso0725@gmail.com>
2026-02-21 11:36:31 +0100

    _ű: added custom fast matrix, decide if we want to use it later

---
+++ VD_marsrover/Project1/file_handler.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "misc.h"
+
+#define SIZE 50
+
+char& u_matrix::operator()(size_t row, size_t col)
+{
+    return data[row * COLS + col];
+}
+
+const char& u_matrix::operator()(size_t row, size_t col) const
+{
+    return data[row * COLS + col];
+}
--- VD_marsrover/Project1/misc.h
+++ VD_marsrover/Project1/misc.h
@@ -1,5 +1,15 @@
 #pragma once
 #include "raylib.h"
+#include <cstdlib>
 
+struct u_matrix {
+	static constexpr size_t ROWS = 50;
+	static constexpr size_t COLS = 50;
 
+	char data[ROWS * COLS];
+
+	char& operator()(size_t row, size_t col);
+
+	const char& operator()(std::size_t row, std::size_t col) const;
+};
 Image load_img_for_icon();
